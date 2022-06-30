48474aa049e8e1d23f2a206481df00d4461aff3b
Carlos-Owens <clojr12@gmail.com>
2022-06-29 21:58:13 -0400

    Test form validation and grid display

--- script.js
+++ script.js
@@ -1,3 +1,80 @@
+let myLibrary = [];
+
+
+
+// Book constructor
+function Books(title, author, pages, status = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+// Add constructor to library array   RETURN TO THIS FUNCTION!!!
+function addBookToLibrary(title, author, pages, status) {
+    let books = new Books(title, author, pages, status);
+    myLibrary.push(books);
+    displayBooks();
+}
+
+function getInput() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const status = document.getElementById("checkbox").value;
+
+    if((title === "") || (author === "") || (pages === "") || (status === "")) {
+       return;
+    } 
+    
+    addBookToLibrary(title, author, pages, status); 
+}
+
+// const 
+
+// Show book cards
+function displayBooks() {
+    const cardDisplay = document.querySelector(".card-display");
+    myLibrary.forEach((book) => {
+        const card = document.createElement("div");
+        const title = document.createElement("p")
+        const author = document.createElement("p");
+        const pages = document.createElement("p")
+        const btnContainer= document.createElement("div");
+        const readBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
+
+        card.classList.add("card");
+        btnContainer.classList.add("btn-container");
+        readBtn.classList.add("btn");
+        deleteBtn.classList.add("btn");
+
+        title.textContent = `"${book.title}"`;
+        author.textContent = `${book.author}`;
+        pages.textContent = `${book.pages} pages`;
+        deleteBtn.textContent = "Remove";
+
+        if(book.status) {
+            readBtn.textContent = "Read"
+            readBtn.classList.add("btn-green");
+        } else {
+            readBtn.textContent = "Not Read";
+            readBtn.classList.add("btn-red");
+        }
+
+
+        cardDisplay.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(btnContainer);
+        btnContainer.appendChild(readBtn);
+        btnContainer.appendChild(deleteBtn);
+
+
+    })
+}
+
 // Form Validation 
 const validateForm = (e) => {
     e.preventDefault();
